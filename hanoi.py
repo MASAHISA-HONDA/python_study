a06c128855dd3dc8d4b4e41d0cc9f64d99aa7a16
mappy_HISAO <m11232mh@gmail.com>
2020-02-23 23:26:15 +0900

    2020/02/23進捗

--- hanoi.py
+++ hanoi.py
@@ -14,10 +14,25 @@ class Stack(Generic[T]):
     def __repr__(self) -> str:
         return repr(self.container)
 
-    num_discs: int =3
-    tower_a :Stack[int] = Stack()
-    tower_b :Stack[int] = Stack()
-    tower_c :Stack[int] = Stack()
-    for i in range(1, num_discs+1):
-        tower_a.push(i)
-    
+print("計算中。。。")
+num_discs: int =30
+tower_a :Stack[int] = Stack()
+tower_b :Stack[int] = Stack()
+tower_c :Stack[int] = Stack()
+for i in range(1, num_discs+1):
+    tower_a.push(i)
+
+def hanoi(begin:Stack[int], end:Stack[int], temp:Stack[int], n:int) -> None:
+    if n == 1:
+        #基底部
+        end.push(begin.pop())
+    else:
+        hanoi(begin,temp,end,n-1)
+        hanoi(begin,end,temp,1)
+        hanoi(temp,end,begin,n-1)
+
+if __name__ == "__main__":
+    hanoi(tower_a,tower_c,tower_b,num_discs)
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
